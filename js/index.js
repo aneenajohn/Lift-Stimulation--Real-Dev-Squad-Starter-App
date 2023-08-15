6a351f6e94cb6b2583a8038f7ff5a7eb48cb6b49
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-08-15 22:30:30 +0530

    Edge cases handled and floor generated dynamically and lift generated as per the hard coded count
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

--- js/index.js
+++ js/index.js
@@ -14,23 +14,99 @@ function cleanupInputs() {
 simulateBtn.addEventListener("click", (e) => {
     e.preventDefault()
     console.log(floors.value, lifts.value)
-    if(!floors.value) {
+    if(!Number(floors.value) || Number(floors.value) < 1) {
         cleanupInputs();
-        alert("Enter number of floors");
-    }else if(Number(floors.value) > 20) {
+        alert("Enter valid number of floors");
+    }else if(!Number(lifts.value) || Number(lifts.value) < 1) {
         cleanupInputs();
-        alert("Max allowed floors: 20")
-    }else if(!lifts.value) {
+        alert("Enter valid number of lifts")
+    }else if(Number(lifts.value) === 1){
         cleanupInputs();
-        alert("Enter number of lifts")
-    }else if(lifts.value > 5) {
+        alert("There should be at least 2 floors");
+    }else if(Number(lifts.value) > 5) {
         alert("Max allowed lifts: 5")
+    }else if(Number(lifts.value) > Number(floors.value)) {
+        alert("Lifts can't be more than the number of floors")
     }else {
         isInputValid = true;
-        simulatedLifts.innerHTML= `<h1 class="font-color-primary">Inputs Validated</h1>`
+        simulatedLifts.innerHTML= `<button class="btn back-btn">Back</button>`
+        userInputPage.style.display = "none";
+        simulatedLifts.style.display =  "block";
 
-        userInputPage.style.display = "none"
+        // DOCS: Back btn to toggle page
+        const backBtn = document.querySelector(".back-btn");
+        backBtn.addEventListener("click", (e) => {
+            simulatedLifts.style.display = "none"
+            userInputPage.style.display = "block"
+            cleanupInputs();
+        })
+
+        generateFloorsAndLifts(floors.value, lifts.value)
     }
 })
 
+function generateFloorsAndLifts (floorCount, liftCount) {
+    const container = document.createElement("div");
+    container.classList.add("container");
 
+    generateLiftsData(liftCount);
+
+    for(let i = 0; i <= floorCount; i++) {
+        const floorContainer = document.createElement("div");
+        floorContainer.classList.add("floor-container");
+        floorContainer.classList.add(`row-${i}`)
+        if(i !== floorCount-1) {
+            floorContainer.style.height = "3.5rem";
+        }
+
+        const lineSplit = document.createElement("div");
+        lineSplit.classList.add("line-split");
+
+        floorContainer.appendChild(lineSplit);
+
+        if(i < floorCount) {
+            const floorContents = document.createElement("div");
+            floorContents.classList.add("floor-contents");
+
+            const btnContainer = document.createElement("div");
+
+            const btnUp = document.createElement("button");
+            btnUp.innerText = "▲"
+            // "⬆️"
+            btnUp.classList.add("lift-btn")
+
+            const btnDown = document.createElement("button");
+            btnDown.innerText = "▼";
+            // ⬇️
+            btnDown.classList.add("lift-btn")
+
+            const lift = document.createElement("div");
+            lift.classList.add("lift");
+            
+            const floorLabel = document.createElement("p");
+            floorLabel.innerText = `Floor ${floorCount - i}`;
+            floorLabel.classList.add("floor-label");
+
+            btnContainer.appendChild(btnUp);
+            btnContainer.appendChild(btnDown);
+            floorContents.appendChild(btnContainer);
+
+            console.log({floorCount , i})
+            console.log(floorCount-1 === i)
+            if(i === floorCount -1) {
+                floorContents.appendChild(lift);
+            }
+            
+            floorContents.appendChild(floorLabel);
+
+            floorContainer.appendChild(floorContents);
+        }
+        container.appendChild(floorContainer);
+        simulatedLifts.appendChild(container);
+    }
+}
+
+function generateLiftsData(lifts) {
+    const liftsData = [];
+
+}
